a09f4410f0941690245661ba28ab42626928aa21
bingenito <bingenito@users.noreply.github.com>
2017-11-16 14:58:35 -0500

    Add ContainerWindow static event framework (events are passed via ipc/messagebus to other windows so events are emitted in each renderer as though they are local)
    container.ts:
    - Refactor move WindowEventArgs to window.ts
    - Simplify static events in Container by updating EventEmitter to have the static implementation rather than use a private instance of EventEmitter within Container.
    
    desktop.ts:
    - Add ContainerWindow class on default export
    
    events.ts:
    - Implement static events on EventEmitter itself.  All static events will be centrally maintained here.
    
    window.ts:
    - Add decorator static event members (add, remove, emit, listeners)

--- src/desktop.ts
+++ src/desktop.ts
@@ -1,11 +1,13 @@
 import { resolveContainer, registerContainer, ContainerRegistration, container } from "./registry";
 import { Container } from "./container";
+import { ContainerWindow } from "./window";
 import * as Default from "./Default/default";
 import * as Electron from "./Electron/electron";
 import * as OpenFin from "./OpenFin/openfin";
 
 const exportDesktopJS = {
     Container,
+    ContainerWindow,
     container,
     registerContainer,
     resolveContainer,
