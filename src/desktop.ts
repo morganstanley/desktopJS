b0b3b5d7349701a1cfb8d4c583a345ef42ee30d9
bingenito <bingenito@users.noreply.github.com>
2017-11-17 14:15:55 -0500

    Remove confusing "container" global export.  The singleton can always be retrieved via resolveContainer().
    Fixes #59

--- src/desktop.ts
+++ src/desktop.ts
@@ -1,4 +1,4 @@
-import { resolveContainer, registerContainer, ContainerRegistration, container } from "./registry";
+import { resolveContainer, registerContainer, ContainerRegistration } from "./registry";
 import { Container } from "./container";
 import { ContainerWindow } from "./window";
 import * as Default from "./Default/default";
@@ -8,7 +8,6 @@ import * as OpenFin from "./OpenFin/openfin";
 const exportDesktopJS = {
     Container,
     ContainerWindow,
-    container,
     registerContainer,
     resolveContainer,
     Default,
