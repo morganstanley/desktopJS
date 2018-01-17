66b090e26a11e842fa6a4bffa6c74c8a4542cd30
Brian Ingenito <bingenito@users.noreply.github.com>
2018-01-17 10:30:28 -0500

    Basic window snapping assist (#99)
    electron.js:
    - Update Electron bootstrap sample to create SnapAssistWindowManager
    
    app.js:
    - Update web sample to create SnapAssistWindowManager when running under OpenFin main window
    
    desktop.ts:
    - Export SnapAssistWindowManager
    
    window.ts:
    - Add core implementation of window snapping via SnapAssistWindowManager.
    
    openfin.ts:
    - When creating new OpenFin windows, fire the created events from the OpenFin callback so that any handlers can access the fully created window.
    
    electron.ts:
    - Change implementation of getBounds to return a Rectangle instead of using duck typing

--- src/desktop.ts
+++ src/desktop.ts
@@ -1,6 +1,6 @@
 import { resolveContainer, registerContainer, ContainerRegistration } from "./registry";
 import { Container } from "./container";
-import { ContainerWindow } from "./window";
+import { ContainerWindow, SnapAssistWindowManager } from "./window";
 import * as Default from "./Default/default";
 import * as Electron from "./Electron/electron";
 import * as OpenFin from "./OpenFin/openfin";
@@ -13,7 +13,8 @@ const exportDesktopJS = {
     resolveContainer,
     Default,
     Electron,
-    OpenFin
+    OpenFin,
+    SnapAssistWindowManager
 };
 
 export default exportDesktopJS; // tslint:disable-line
