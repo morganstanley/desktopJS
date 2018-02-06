5b1c581c37c093d13066b2ff4733ae90e3ae7850
Brian Ingenito <bingenito@users.noreply.github.com>
2018-02-06 12:04:00 -0500

    Add ContainerNotification and ObjectTransform to default exports.  This will allow for easier custom container implementations from class reuse. (#123)

--- src/desktop.ts
+++ src/desktop.ts
@@ -1,5 +1,7 @@
 import { resolveContainer, registerContainer, ContainerRegistration } from "./registry";
 import { Container } from "./container";
+import { ContainerNotification } from "./notification";
+import { ObjectTransform } from "./propertymapping";
 import { ContainerWindow, SnapAssistWindowManager } from "./window";
 import * as Default from "./Default/default";
 import * as Electron from "./Electron/electron";
@@ -11,6 +13,8 @@ export default class Desktop { //tslint:disable-line
     static get ContainerWindow(): typeof ContainerWindow { return ContainerWindow; }
     static get registerContainer(): typeof registerContainer { return registerContainer; }
     static get resolveContainer(): typeof resolveContainer { return resolveContainer; }
+    static get ContainerNotification(): typeof ContainerNotification { return ContainerNotification; }
+    static get ObjectTransform(): typeof ObjectTransform { return ObjectTransform; }
     static get Default(): typeof Default { return Default; }
     static get Electron(): typeof Electron { return Electron; }
     static get OpenFin(): typeof OpenFin { return OpenFin; }
