febf40c0b034a3f4a7bc418eba74a114e85c160b
Brian Ingenito <Brian.Ingenito@morganstanley.com>
2019-01-03 15:30:26 -0500

    Fix breaking gulp 4 changes for tasks

--- .gulp/tasks/dts.js
+++ .gulp/tasks/dts.js
@@ -4,13 +4,15 @@ var dts = require('dts-bundle');
 
 module.exports = function (gulp, name, main, out) {
     return function () {
-        return dts.bundle({
+        dts.bundle({
             name: name,
             main: main,
             out: out,
             verbose: false,
             outputAsModuleFolder: true
         });
+
+        return Promise.resolve();
     };
 }
 
