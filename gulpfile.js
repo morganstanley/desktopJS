c3e37cc0b382698b79c5b417df7651733b872780
Dan Jasnowski <djasnowski@gmail.com>
2019-02-07 12:32:25 -0600

    Replace gulp-webserver with gulp-connect. Fixes #195.

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,20 @@
 'use strict';
 
 var gulp = require('gulp'),
-    webserver = require('gulp-webserver'),
+    connect = require('gulp-connect'),
     gulpConfig = require('./.gulp/gulpConfig');
 
 gulp.task('test', require('./.gulp/tasks/tests')(gulp, gulpConfig));
 
 gulp.task("server", function () {
+	connect.server({
+		root: 'examples/web/',
+		port: 8000,
+		livereload: true
+  	});
+
     return gulp.src(['examples/web', 'packages/desktopjs/dist', 'packages/desktopjs-electron/dist', 'packages/desktopjs-openfin/dist'])
-        .pipe(webserver({
-            fallback: 'index.html',
-            livereload: {
-                enable: true
-            }
-        }));
+        .pipe(connect.reload());
 });
 
 gulp.task('docs', require('./.gulp/tasks/docs')(gulp, gulpConfig));
