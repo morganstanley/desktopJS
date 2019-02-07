9bbbc3409454438ffdbeaf64a185d557d320565e
Dan Jasnowski <djasnowski@gmail.com>
2019-02-07 12:32:25 -0600

    Covered by dco/daniel-jasnowski-pascual.md

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,17 @@
 'use strict';
 
 var gulp = require('gulp'),
-    webserver = require('gulp-webserver'),
+    connect = require('gulp-connect'),
     gulpConfig = require('./.gulp/gulpConfig');
 
 gulp.task('test', require('./.gulp/tasks/tests')(gulp, gulpConfig));
 
 gulp.task("server", function () {
-    return gulp.src(['examples/web', 'packages/desktopjs/dist', 'packages/desktopjs-electron/dist', 'packages/desktopjs-openfin/dist'])
-        .pipe(webserver({
-            fallback: 'index.html',
-            livereload: {
-                enable: true
-            }
-        }));
+	connect.server({
+		root: ['examples/web', 'packages/desktopjs/dist', 'packages/desktopjs-electron/dist', 'packages/desktopjs-openfin/dist'],
+		port: 8000,
+		livereload: true
+  	});
 });
 
 gulp.task('docs', require('./.gulp/tasks/docs')(gulp, gulpConfig));
