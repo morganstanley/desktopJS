04dbafd9a8b7dc916f89f72a15af778c279510e3
Dan Jasnowski <djasnowski@gmail.com>
2019-02-11 11:29:02 -0600

    Remove return and have array as root

--- gulpfile.js
+++ gulpfile.js
@@ -8,13 +8,10 @@ gulp.task('test', require('./.gulp/tasks/tests')(gulp, gulpConfig));
 
 gulp.task("server", function () {
 	connect.server({
-		root: 'examples/web/',
+		root: ['examples/web', 'packages/desktopjs/dist', 'packages/desktopjs-electron/dist', 'packages/desktopjs-openfin/dist'],
 		port: 8000,
 		livereload: true
   	});
-
-    return gulp.src(['examples/web', 'packages/desktopjs/dist', 'packages/desktopjs-electron/dist', 'packages/desktopjs-openfin/dist'])
-        .pipe(connect.reload());
 });
 
 gulp.task('docs', require('./.gulp/tasks/docs')(gulp, gulpConfig));
