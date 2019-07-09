9fb3b174c37ddf2074d1148bfa594a997360618f
Brian Ingenito <28159742+bingenito@users.noreply.github.com>
2019-07-09 15:06:47 -0400

    Fix code codcov integration (#258)
    * Update codecov to 3.5.0
    * Strip 'build' dir from source maps in coverage file

--- .gulp/tasks/tests.js
+++ .gulp/tasks/tests.js
@@ -2,6 +2,7 @@
 
 var jasmine = require('gulp-jasmine'),
     istanbul = require('gulp-istanbul'),
+    replace = require('gulp-replace'),
     remap = require('remap-istanbul/lib/gulpRemapIstanbul'),
     instanbulEnforcer = require('gulp-istanbul-enforcer');
 
@@ -10,38 +11,49 @@ module.exports = function (gulp, config) {
         return gulp.src(config.test.coverage.src)
             .pipe(istanbul({ includeUntested: true }))
             .pipe(istanbul.hookRequire())
-            .on('finish', function () {
+            .on('finish', () => {
+
+                // Run jasmine under istanbul
                 gulp.src(config.test.src)
                     .pipe(jasmine({ verbose: true, errorOnFail: true, includeStackTrace: false }))
                     .pipe(istanbul.writeReports({
                         dir: config.test.coverage.dest,
                         reporters: ['json']
                     }))
-                    .on('end', remapCoverageFiles)
-                    .on('finish', function () {
-                        gulp.src('.')
-                            .pipe(instanbulEnforcer({
-                                coverageDirectory: config.test.coverage.dest,
-                                rootDirectory: '',
-                                thresholds: {
-                                    statements: config.test.coverage.threshold || 80
-                                }
-                            }));
-                    });
-            });
 
-        /** Take js coverage json and remap to typescript.  Output html and text */
-        function remapCoverageFiles() {
-            return gulp.src(config.test.coverage.coverageFile)
-                .pipe(remap({
-                    reports: {
-                        'json': config.test.coverage.coverageFile, // overwrite js based json with ts remapped version
-                        'html': config.test.coverage.dest,
-                        'lcovonly': config.test.coverage.lcovFile,
-                        'text': null
-                    }
-                }));
-        };
+                    // Take js coverage json and remap to typescript.  Output html and text
+                    .on('end', () => {
+                        return gulp.src(config.test.coverage.coverageFile)
+                            .pipe(remap({
+                                reports: {
+                                    'json': config.test.coverage.coverageFile, // overwrite js based json with ts remapped version
+                                    'html': config.test.coverage.dest,
+                                    'lcovonly': config.test.coverage.lcovFile,
+                                    'text': null
+                                }
+                            }))
+
+                            // Remove staging build directory in code coverage for correct file mapping
+                            .on('finish', function () {
+                                return gulp.src(config.test.coverage.coverageFile)
+                                    .pipe(replace(/\\\\build\\\\src/mg, '\\\\src')) // windows
+                                    .pipe(replace(/\/build\/src/mg, '/src')) // unix)
+                                    .pipe(gulp.dest(config.test.coverage.dest))
+
+                                    // Enforce statement coverage threshold based on config or if not defined 80%
+                                    .on('finish', function () {
+                                        return gulp.src('.')
+                                            .pipe(instanbulEnforcer({
+                                                coverageDirectory: config.test.coverage.dest,
+                                                rootDirectory: '',
+                                                thresholds: {
+                                                    statements: config.test.coverage.threshold || 80
+                                                }
+                                            }));
+                                    });
+                            })
+                    })
+            });
     };
 }
 
